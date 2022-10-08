4bf4816c74e79e2996a99be2260ed37104b11268
Alexander Schlieden <alex.schlieden@gmail.com>
2022-10-08 19:37:06 +0200

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galeryEl = document.querySelector('.gallery');
+
+const galleryImgMarkup = createGaleryItem(galleryItems);
+
+galeryEl.insertAdjacentHTML('beforeend', galleryImgMarkup);
+
+function createGaleryItem(galeryItem) {
+  return galeryItem
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
